2e0cb4720589981359b66805a63bb8fce39f1cff
Mr Pinguiiin <52125490+MrPinguiiin@users.noreply.github.com>
2024-05-26 14:10:58 +0700

    add static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,19 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+    kit: {
+        adapter: adapter({
+            // default options are shown
+            pages: 'build2',
+            assets: 'build2',
+            fallback: null,
+            precompress: false
+        }),
+        // Add the following line to support SPA fallback for GitHub Pages
+        prerender: {
+            default: true
+        }
+    }
 };
 
 export default config;
